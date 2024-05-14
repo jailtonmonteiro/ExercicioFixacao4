4fed4fa56b0ba9e51e0082f3a803e3863b333038
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-05-13 22:37:02 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFixacao4/Entities/Order.cs
@@ -0,0 +1,36 @@
+﻿using ExercicioFixacao4.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacao4.Entities
+{
+    internal class Order
+    {
+        public DateTime Momment {  get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double sum = 0;
+
+            foreach (OrderItem item in Items)
+            {
+                sum = +item.Price;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ ExercicioFixacao4/Entities/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacao4.Entities
+{
+    internal class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public double subTotal()
+        {
+            return Price;
+        }
+
+        public OrderItem() { }
+
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
